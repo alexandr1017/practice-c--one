bc25e6cadb98fccc0eafd05e9e12e180a7367b46
alexandr1017 <alexandr1993228@yandex.ru>
2024-09-08 18:45:14 +0300

    add test button with showInfo for Person class

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
+       
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,16 +17,38 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
+      
         private void InitializeComponent()
         {
+
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
+
+            
+            // Создаем метку (Label)
+            Label label = new Label();
+            label.Location = new Point(100, 100); // Позиция метки на форме
+            label.AutoSize = true; // Автоматическое изменение размера метки в зависимости от длины текста
+
+            Button showInfoButton = new Button();
+            showInfoButton.Text = "Show Person Info";
+            showInfoButton.Location = new Point(150,150);
+            showInfoButton.Click += (sender, e) => ShowPersonInfo(label);
+
+            // Добавляем метку на форму
+            this.Controls.Add(label);
+            this.Controls.Add(showInfoButton);
+
+        }
+        private void ShowPersonInfo(Label label)
+        {
+            // Создаем объект Person
+            Person person = new Person(30, "Alex");
+
+            // Выводим информацию в метку
+            label.Text = person.printInfo();
         }
 
         #endregion
---
+++ WinFormsApp1/WinFormsApp1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class Person
+    {
+        String name;
+        int age;
+
+        public Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+       public String printInfo() {
+           return name + " " + age;
+        }
+
+    }
+}

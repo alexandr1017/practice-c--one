10a343c52caf34d7727b8627a23e030853dd3c4d
alexandr1017 <alexandr1993228@yandex.ru>
2024-09-20 12:05:24 +0300

    Задокументирован класс MainOfMainForm

--- WinFormsApp1/WinFormsApp1/MainOfMainForm.cs
+++ WinFormsApp1/WinFormsApp1/MainOfMainForm.cs
@@ -5,23 +5,31 @@ using System.Windows.Forms;
 
 namespace NoteApp
 {
+    /// <summary>
+    /// Главный класс, содержащий точку входа в приложение NoteApp.
+    /// </summary>
     static class MainOfMainForm
     {
+        /// <summary>
+        /// Основной метод, с которого начинается выполнение приложения.
+        /// </summary>
         [STAThread]
         static void Main()
         {
+            // Включение визуальных стилей для приложения (соответствие современным темам Windows).
             Application.EnableVisualStyles();
+
+            // Установка совместимости для рендеринга текста.
             Application.SetCompatibleTextRenderingDefault(false);
 
-            
-
+            // Загрузка проекта из файла JSON с помощью ManagerProject и статаического метода класса loadProjectFromJsonFile().
             Project project = ManagerProject.loadProjectFromJsonFile();
-           
+
+            // Создание главной формы приложения и передача в нее загруженного Project.
             MainForm mainForm = new MainForm(project);
-            
+
+            // Запуск главной формы и начало работы приложения.
             Application.Run(mainForm);
-
-
         }
     }
 }

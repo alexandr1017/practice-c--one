aebd855c4a55431c97bdc76f45a65e6a07056564
alexandr1017 <alexandr1993228@yandex.ru>
2024-09-20 12:01:40 +0300

    Задокументирован класс AboutForm

--- WinFormsApp1/WinFormsApp1/AboutForm.cs
+++ WinFormsApp1/WinFormsApp1/AboutForm.cs
@@ -1,12 +1,21 @@
 ﻿namespace NoteApp
 {
+    /// <summary>
+    /// Форма, отображающая информацию о программе.
+    /// </summary>
     public partial class AboutForm : Form
     {
+        /// <summary>
+        /// Конструктор по умолчанию для AboutForm.
+        /// </summary>
         public AboutForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Инициализация компонентов формы.
+        /// </summary>
         private void InitializeComponent()
         {
             aboutLabel = new Label();
@@ -14,6 +23,9 @@
             // 
             // aboutLabel
             // 
+            /// <summary>
+            /// Метка, отображающая информацию о версии и авторе приложения.
+            /// </summary>
             aboutLabel.AutoSize = true;
             aboutLabel.Location = new Point(10, 10);
             aboutLabel.Name = "aboutLabel";
@@ -24,6 +36,9 @@
             // 
             // AboutForm
             // 
+            /// <summary>
+            /// Устанавливаются параметры формы, такие как размер окна, заголовок и стартовая позиция.
+            /// </summary>
             ClientSize = new Size(384, 161);
             Controls.Add(aboutLabel);
             Name = "AboutForm";
@@ -34,16 +49,25 @@
             this.StartPosition = FormStartPosition.CenterScreen;
         }
 
+        /// <summary>
+        /// Событие, вызываемое при нажатии на метку aboutLabel.
+        /// </summary>
         private void aboutLabel_Click(object sender, EventArgs e)
         {
-
+            // Логика при клике на метку (пока не реализовано).
         }
 
+        /// <summary>
+        /// Метка, отображающая информацию о программе.
+        /// </summary>
         private Label aboutLabel;
 
+        /// <summary>
+        /// Событие, вызываемое при загрузке формы AboutForm.
+        /// </summary>
         private void AboutForm_Load(object sender, EventArgs e)
         {
-
+            // Логика при загрузке формы (пока не реализовано).
         }
     }
 }
